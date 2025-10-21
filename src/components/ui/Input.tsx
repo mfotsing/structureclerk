74e6efe41362112532e4d594e148956655438600
mfotsing <mfotsing@users.noreply.github.com>
2025-10-21 09:35:03 -0400

    Fix ESLint errors: escape apostrophes and fix React hook usage

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -11,7 +11,8 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, label, error, helperText, leftIcon, rightIcon, id, ...props }, ref) => {
-    const inputId = id || `input-${React.useId()}`
+    const generatedId = React.useId()
+    const inputId = id || `input-${generatedId}`
     
     return (
       <div className="space-y-2">
